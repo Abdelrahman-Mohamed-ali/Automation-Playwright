c8d51c1e24af32321badebbbeeb3001686e6ff31
Abdelrahman Mohammed Ali <abdelrahmanmohammed220@gmail.com>
2026-09-14 18:40:20 +0300

    Fix missing code

--- tests/shop.spec.ts
+++ tests/shop.spec.ts
@@ -11,6 +11,8 @@ test("Should add specific product to cart successfully" , async ({ page }) => {
 
 const search = new ShopPage(page)
 await search.open()
+await search.Fill()
+await expect(page.getByLabel("Success! The Form has been submitted successfully!.")).toBeVisible
 
 await search.openShop()
 await expect(page.locator("a").filter({ hasText: "Checkout" })).toContainText("Checkout ( 2 )");
